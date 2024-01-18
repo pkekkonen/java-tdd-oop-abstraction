ecb5e1eb3b2d12758208450f99f46c384efe2c83
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-18 11:09:37 +0100

    createUserValidPassword passed

--- src/main/java/com/booleanuk/core/System.java
+++ src/main/java/com/booleanuk/core/System.java
@@ -2,6 +2,13 @@ package com.booleanuk.core;
 
 public class System {
     public String createUser(String email, String password) {
-        return "invalid password";
+        if(!isPasswordValid(password)) {
+            return "invalid password";
+        }
+        return "User created";
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() >= 8;
     }
 }
