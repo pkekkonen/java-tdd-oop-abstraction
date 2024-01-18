48355d3d575015c8dca39003570cf96f9cd88c2f
pkekkonen <paulina.kekkonen@hotmail.com>
2024-01-18 11:06:17 +0100

    createUserValidPassword added

--- src/test/java/com/booleanuk/core/SystemTest.java
+++ src/test/java/com/booleanuk/core/SystemTest.java
@@ -15,4 +15,13 @@ public class SystemTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void createUserValidPassword() {
+        System system = new System();
+        String email = "bob@gmail.com";
+        String password = "12345678";
+        String actual = system.createUser(email, password);
+        String expected = "User created";
+        Assertions.assertEquals(expected, actual);
+    }
 }
